ff0227f5392f93fb6cbeef607116350762e79b3d
Fortune Mahloane <sensefortune01@gmail.com>
2025-11-28 22:34:33 +0200

    Add lastLoginAt field to user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -45,6 +45,10 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    lastLoginAt: {
+      type: Date,
+      default: null,
+    },
     verificationToken: {
       type: String,
       select: false,
