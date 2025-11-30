33a7a76481ef349ddf8d8bbc51cc67412ada1b79
Fortune Mahloane <sensefortune01@gmail.com>
2025-11-30 23:40:52 +0200

    hardcode backend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,15 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default ({ mode }) => {
-
-  //const env = loadEnv(mode, process.cwd(), '') 
-
-  return defineConfig({
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: 'https://shukuma-exercise-app.onrender.com',
-          changeOrigin: true,
-          secure: false,
-        },
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "https://shukuma-exercise-app.onrender.com/",
+        changeOrigin: true,
       },
     },
-  })
-}
+  },
+});
