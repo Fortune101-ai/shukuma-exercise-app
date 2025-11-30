28b18bfb6c0a9f3fe62f34b3834f757bfa889b5e
Fortune Mahloane <sensefortune01@gmail.com>
2025-11-30 22:42:09 +0200

    fix env import

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_API_URL,
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
-})
+export default ({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), '') 
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  })
+}
